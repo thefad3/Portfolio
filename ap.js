44bb55f23292000ea0671356871447dd646b34ee
Chris Lynch <chrislynch@Chriss-MacBook-Pro-2.local>
2015-08-10 18:39:49 -0400

    This

---
+++ ap.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use('/client', express.static('client'));
+
+
+
+app.get('/', function (req, res) {
+    res.render('home');
+
+});
+
+
+app.listen(process.env.PORT || 9000, process.env.IP || "0.0.0.0", function(){
+    console.log("Its listening over 9000");
+});
