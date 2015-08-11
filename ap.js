68da0019a28787045c019fcba6ed02f1a4fdb711
Chris Lynch <chrislynch@Chriss-MacBook-Pro-2.local>
2015-08-10 23:21:18 -0400

    Errr

--- ap.js
+++ ap.js
@@ -3,13 +3,11 @@ var express = require('express'),
     app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use('/client', express.static('client'));
-
+app.use('client', express.static('client'));
 
 
 app.get('/', function (req, res) {
     res.render('home');
-
 });
 
 
